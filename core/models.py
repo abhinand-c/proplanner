dcd3fe7c970ddd1ae4e8b690b745c066a72e9491
Abhinand C <abhinandc.ac@gmail.com>
2021-05-08 11:26:35 +0530

    updated user model, added skills, role models

--- core/models.py
+++ core/models.py
@@ -3,5 +3,20 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+class Skill(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
 class User(AbstractUser):
-    pass
+    class RoleLevel(models.IntegerChoices):
+        JR = 0, 'Junior'
+        MID = 1, 'Mid'
+        SR = 2, 'Senior'
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
+    role_level = models.IntegerField(choices=RoleLevel.choices, default=RoleLevel.JR)
+    skills = models.ManyToManyField(Skill, blank=True)
+    interests = models.ManyToManyField(Skill, blank=True, related_name="skill_interests")
