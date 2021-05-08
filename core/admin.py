3553223c73fe0813c2fbc31e36ea59ed029d20e1
Abhinand C <abhinandc.ac@gmail.com>
2021-05-08 14:09:20 +0530

    Registered model in admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+admin.site.site_header = "Admin Management System"
+admin.site.index_title = "Welcome To Admin Management"
+admin.site.site_title = "Primary Control Hub"
+
+
+admin.site.register(models.User)
+admin.site.register(models.Role)
+admin.site.register(models.Skill)
