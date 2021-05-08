dd9e8484700965cf9e70233bb0d19e5d312f6741
Abhinand C <abhinandc.ac@gmail.com>
2021-05-08 23:08:46 +0530

    TAsk create & project create form

---
+++ apps/task/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from . import models
+
+class CreateTaskForm(forms.ModelForm):
+    class Meta:
+        model = models.Task
+        fields = ['title', 'description', 'skill', 'duration', 'deadline' ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description' : forms.Textarea(attrs={'class': 'form-control', 'rows':'3'}),
+            'deadline' : forms.DateTimeInput(attrs={'class': 'form-control datepicker', 'placeholder':'YYYY-MM-DD'}),
+        }
+
+class CreateProjectForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['title', 'description', 'start_date', 'end_date' ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description' : forms.Textarea(attrs={'class': 'form-control', 'rows':'3'}),
+            'start_date' : forms.DateTimeInput(attrs={'class': 'form-control datepicker', 'placeholder':'YYYY-MM-DD'}),
+            'end_date' : forms.DateTimeInput(attrs={'class': 'form-control datepicker', 'placeholder':'YYYY-MM-DD'}),
+        }
